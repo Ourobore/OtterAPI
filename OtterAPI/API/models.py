001c47bfbdcbf82ce4111428680c30e812d90273
Ourobore <lena.chaprenet@gmail.com>
2022-07-28 08:17:37 +0000

    Added API to admin panel, added fields to Otter entity

--- OtterAPI/API/models.py
+++ OtterAPI/API/models.py
@@ -1,8 +1,25 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Otter(models.Model):
     def __str__(self):
         return self.name
 
+    class Sex(models.TextChoices):
+        MALE = "M", "Male"
+        FEMALE = "F", "Female"
+
     name = models.CharField(max_length=12)
+    sex = models.CharField(
+        max_length=5,
+        choices=Sex.choices,
+        default=Sex.FEMALE,
+    )
+    age = models.SmallIntegerField(
+        default=0,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(25),
+        ],
+    )
